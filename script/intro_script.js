6f0dd31af5d05928f2598595f2f3e767747725d0
Kelly G Joseph <seisolo76@gmail.com>
2020-11-10 19:22:55 -0700

    Update intro_script.js

--- script/intro_script.js
+++ script/intro_script.js
@@ -1,19 +1,42 @@
-https://cdnjs.cloudflare.com/ajax/libs/gsap/3.2.4/gsap.min.js
-https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/GSDevTools3.min.js
-https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/SplitText3.min.js
-
-gsap.registerPlugin(GSDevTools, SplitText)
-let split
-let animation = gsap.timeline({})
-
-function init() {
-	gsap.set(".wrapper", {autoAlpha:1})
-	split = new SplitText("h1", {type:"chars"})
-	animation.from(split.chars, {opacity:0, y:50, ease:"back(8)", stagger:{
-		from:"front",
-		each:0.2
-	}})
-	GSDevTools.create({animation:animation})
+// set up text to print, each item in array is new line
+var aText = new Array(
+  "Q: What is Computational Sustainability?", 
+  "A:Computational Sustainability is an interdisciplinary field that aims to apply techniques from computer science, information science, operations research, applied mathematics, and statistics for balancing environmental, economic, and societal needs for sustainable development (Gomez and Sabharwal).",
+  "Q: Why Computational Sustainability and not another topic?",
+  "A: I chose computational sustainability as my topic because it relates to my major. I am currently a computer science major. When I was first thinking about what I wanted my topic to be I had a couple initial ideas, but none were really sticking. I decided to see if there was any area of environmental stewardship that had to do with computer science and that's how I found out about computational sustainability. The more I read into computational sustainability then more I knew that this is what my topic should be. It involved something I am passionate about so it was the perfect topic for me.",
+  "Q: Why should you care about Computational Sustainability?",
+  "A: There are so many interesting applications and research being done in this field. It has so much potential to do good and help better the planet. It may sound boring and sound like it is just a bunch of technical jargon, but it is more than that!"	
+);
+var iSpeed = 75; // time delay of print out
+var iIndex = 0; // start printing array at this posision
+var iArrLength = aText[0].length; // the length of the text array
+var iScrollAt = 20; // start scrolling up at this many lines
+ 
+var iTextPos = 0; // initialise text position
+var sContents = ''; // initialise contents variable
+var iRow; // initialise current row
+ 
+function typewriter()
+{
+ sContents =  ' ';
+ iRow = Math.max(0, iIndex-iScrollAt);
+ var destination = document.getElementById("typedtext");
+ 
+ while ( iRow < iIndex ) {
+  sContents += aText[iRow++] + '<br />';
+ }
+ destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+ if ( iTextPos++ == iArrLength ) {
+  iTextPos = 0;
+  iIndex++;
+  if ( iIndex != aText.length ) {
+   iArrLength = aText[iIndex].length;
+   setTimeout("typewriter()", 500);
+  }
+ } else {
+  setTimeout("typewriter()", iSpeed);
+ }
 }
 
-window.addEventListener("load", init)
+
+typewriter();
