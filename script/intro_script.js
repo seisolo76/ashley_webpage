f0d58bc9e9af2cda66c9604327716f3e3a9f51b4
Kelly G Joseph <seisolo76@gmail.com>
2020-11-10 19:38:06 -0700

    Update intro_script.js

--- script/intro_script.js
+++ script/intro_script.js
@@ -1,12 +1,15 @@
 // set up text to print, each item in array is new line
 var aText = new Array(
   "Q: What is Computational Sustainability?", 
-  <br>,
+  "/n",
   "A: \"Computational Sustainability is an interdisciplinary field that aims to apply techniques from computer science, information science, operations research, applied mathematics, and statistics for balancing environmental, economic, and societal needs for sustainable development\" (Gomez and Sabharwal).",
-  <br>,
+  "/n",
   "Q: Why Computational Sustainability and not another topic?",
+  "/n",
   "A: I chose computational sustainability as my topic because it relates to my major. I am currently a computer science major. When I was first thinking about what I wanted my topic to be I had a couple initial ideas, but none were really sticking. I decided to see if there was any area of environmental stewardship that had to do with computer science and that's how I found out about computational sustainability. The more I read into computational sustainability then more I knew that this is what my topic should be. It involved something I am passionate about so it was the perfect topic for me.",
+  "/n",
   "Q: Why should you care about Computational Sustainability?",
+  "/n",
   "A: There are so many interesting applications and research being done in this field. It has so much potential to do good and help better the planet. It may sound boring and sound like it is just a bunch of technical jargon, but it is more than that!"	
 );
 var iSpeed = 50; // time delay of print out
